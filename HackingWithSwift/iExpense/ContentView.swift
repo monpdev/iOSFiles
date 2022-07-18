2b6241734f8f65984bf8945dddbc314fe6dd818f
monpdev <35469743+monpdev@users.noreply.github.com>
2022-07-18 10:23:14 +0100

    Create ContentView.swift

---
+++ HackingWithSwift/iExpense/ContentView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                List{
+                    ForEach(numbers, id:\.self){
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                Button("Add Number"){
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                    }
+                }
+            .navigationTitle("onDelete()")
+            .toolbar{
+                    EditButton()
+                }
+            }
+        }
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+}
+    
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
