3cd6db1afe6caed8a392ba6aaa8a3d60ab8c621f
monpdev <35469743+monpdev@users.noreply.github.com>
2022-07-20 09:10:38 +0100

    Update ContentView.swift

--- HackingWithSwift/iExpense/ContentView.swift
+++ HackingWithSwift/iExpense/ContentView.swift
@@ -1,37 +1,58 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Mon P. on 17/07/2022.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     
     var body: some View {
-        NavigationView{
-            VStack{
-                List{
-                    ForEach(numbers, id:\.self){
-                        Text("Row \($0)")
+        NavigationView {
+            List{
+                ForEach(expenses.items){item in
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: "USD"))
                     }
-                    .onDelete(perform: removeRows)
+                    
                 }
-                Button("Add Number"){
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                    }
-                }
-            .navigationTitle("onDelete()")
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
             .toolbar{
-                    EditButton()
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                    
                 }
             }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
         }
-    func removeRows(at offsets: IndexSet){
-        numbers.remove(atOffsets: offsets)
     }
 }
+
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+    }
     
+}
 
-
-
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
