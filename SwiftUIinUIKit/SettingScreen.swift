8128d2d278afe04a9035d8bce2461a810048a100
monpdev <35469743+monpdev@users.noreply.github.com>
2022-08-31 16:27:16 +0100

    Update SettingScreen.swift

--- SwiftUIinUIKit/SettingScreen.swift
+++ SwiftUIinUIKit/SettingScreen.swift
@@ -1,31 +1,42 @@
 //
-//  ViewController.swift
+//  SettingScreen.swift
 //  SwiftUIinUIKit
 //
 //  Created by Mon P. on 31/08/2022.
 //
 
-import UIKit
 import SwiftUI
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 50))
-        view.addSubview(button)
-        button.center = view.center
-        button.setTitle("Show SwiftUI", for: .normal)
-        button.backgroundColor = .systemPink
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-    }
-        
-        @objc func didTapButton() {
-            //SwiftUI interop
-            let vc = UIHostingController(rootView: SettingScreen())
-            present(vc, animated: true)
+struct SettingScreen: View {
+    @State var isOn = false
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Form{
+                    Toggle(isOn: $isOn, label: {
+                        Text("Is Subscriber")
+                    })
+                    
+                    Toggle(isOn: $isOn, label: {
+                        Text("Is Member")
+                    })
+                    
+                    Toggle(isOn: $isOn, label: {
+                        Text("Is Follower")
+                    })
+                    
+                }
+                
+             
+            }
+            .navigationTitle("Setting")
         }
-        
+    }
 }
 
+struct SettingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingScreen()
+    }
+}
