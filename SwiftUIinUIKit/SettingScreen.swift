cb4cfc9fe04a1d183b61dd6be77f6668ad9190f6
monpdev <35469743+monpdev@users.noreply.github.com>
2022-08-31 16:26:15 +0100

    Create SettingScreen.swift

---
+++ SwiftUIinUIKit/SettingScreen.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SwiftUIinUIKit
+//
+//  Created by Mon P. on 31/08/2022.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Show SwiftUI", for: .normal)
+        button.backgroundColor = .systemPink
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+        
+        @objc func didTapButton() {
+            //SwiftUI interop
+            let vc = UIHostingController(rootView: SettingScreen())
+            present(vc, animated: true)
+        }
+        
+}
+
